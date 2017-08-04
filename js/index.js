664c952f0b6ee54538783fbac1a6b23fe22c2106
Raul Zuniga <rzuniga64@gmail.com>
2017-08-04 01:54:01 -0500

    Update and add more products.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
+$(document).ready(function () {
 
+    /*var chartData = [];
+
+    d3.csv('garbage.csv', function(data) {
+        //console.log(data);
+    });
+
+    $.getJSON('user_sample.json')
+        .done(function(data) {
+            chartData = data;
+        });*/
+});
